2c04bcf1c81c1dc28b802fbeb1df702c652723db
Doston Shernazarov <98680065+dostonshernazarov@users.noreply.github.com>
2024-03-06 12:29:44 +0500

    Update app.py
    check image is animal?

--- app.py
+++ app.py
@@ -17,35 +17,34 @@ if file:
     checkModel = load_learner('animals_check_model.pkl')
     
     pCheck, pidCheck, probCheck = checkModel.predict(img)
-    st.success(f"Predict: {pCheck}")
-    st.info(f"Probability: {probCheck[pidCheck]*100:.1f}%")
+    if pCheck == 'Bird_Insect_Carnivore_Fish_Reptile' :
     
-    # Model
-    model = load_learner('animals_model.pkl')
+        # Model
+        model = load_learner('animals_model.pkl')
 
-    # Predict 
-    pred, pred_id, probs = model.predict(img)
+        # Predict 
+        pred, pred_id, probs = model.predict(img)
 
-    st.success(f"Predict: {pred}")
-    st.info(f"Probability: {probs[pred_id]*100:.1f}%")
+        st.success(f"Predict: {pred}")
+        st.info(f"Probability: {probs[pred_id]*100:.1f}%")
 
-    # Plotting
-    fig = px.bar(x=model.dls.vocab,y= probs*100, title="Animal Classification Sort")
-    fig.update_layout(
-        autosize=False,
-        width=600,
-        height=450,
-        template='plotly_dark',
-        title=dict(
-            text="Animal Classification Sort",
-            font=dict(size=20, color='#FFFFFF'),
-            x=0.4,
-            y=0.9
-        ),
-        xaxis_title=dict(text='Classification', font=dict(size=16, color='#FFFFFF')),
-        yaxis_title=dict(text='Probability', font=dict(size=16, color='#FFFFFF')),
-        plot_bgcolor='rgb(40, 70, 70)',
-        xaxis=dict(tickfont=dict(size=14, color='#FFFFFF')),
-        yaxis=dict(tickfont=dict(size=14, color='#FFFFFF'))
-    )    
-    st.plotly_chart(fig)
+        # Plotting
+        fig = px.bar(x=model.dls.vocab,y= probs*100, title="Animal Classification Sort")
+        fig.update_layout(
+            autosize=False,
+            width=600,
+            height=450,
+            template='plotly_dark',
+            title=dict(
+                text="Animal Classification Sort",
+                font=dict(size=20, color='#FFFFFF'),
+                x=0.4,
+                y=0.9
+            ),
+            xaxis_title=dict(text='Classification', font=dict(size=16, color='#FFFFFF')),
+            yaxis_title=dict(text='Probability', font=dict(size=16, color='#FFFFFF')),
+            plot_bgcolor='rgb(40, 70, 70)',
+            xaxis=dict(tickfont=dict(size=14, color='#FFFFFF')),
+            yaxis=dict(tickfont=dict(size=14, color='#FFFFFF'))
+        )    
+        st.plotly_chart(fig)
